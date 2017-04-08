740ba85c84ee768bf00daef2a9bc305c13ce0a53
Alvin Chau <alvinchau2013@gmail.com>
2017-04-08 19:11:35 -0400

    added album controller

---
+++ src/application/AlbumController.java
@@ -0,0 +1,49 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class AlbumController {
+	
+	
+	@FXML
+    private Button albumCreate;
+	
+	@FXML
+    private Button albumOpen;
+	
+	@FXML
+    private Button albumDelete;
+	
+	@FXML
+    private Button albumRename;
+
+    @FXML
+    private TextField albumField;
+	
+	
+	public void handleRename()
+	{
+		
+		
+	}
+	
+	public void handleCreate()
+	{
+		
+		
+	}
+	
+	public void handleDelete()
+	{
+		
+	}
+	
+	public void handleOpen()
+	{
+		
+		
+	}
+
+}
